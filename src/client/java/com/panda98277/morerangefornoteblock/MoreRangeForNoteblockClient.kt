016cc0332a8674c2a3b4b0e4b59859044341d155
Panda98277 <553432805@qq.com>
2024-02-08 10:34:29 +0800

    !!!

---
+++ src/client/java/com/panda98277/morerangefornoteblock/MoreRangeForNoteblockClient.kt
@@ -0,0 +1,9 @@
+package com.panda98277.morerangefornoteblock
+
+import net.fabricmc.api.ClientModInitializer
+
+class MoreRangeForNoteblockClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/panda98277/morerangefornoteblock/MoreRangeForNoteblock.kt
@@ -0,0 +1,9 @@
+package com.panda98277.morerangefornoteblock
+
+import net.fabricmc.api.ModInitializer
+
+class MoreRangeForNoteblock : ModInitializer  {
+    override fun onInitialize() {
+
+    }
+}
