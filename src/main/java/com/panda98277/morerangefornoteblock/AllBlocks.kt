33c4174b88654845ca013b79bed7491287bf2d54
Panda98277 <553432805@qq.com>
2024-02-12 20:59:46 +0800

    Finally succeed

--- src/main/java/com/panda98277/morerangefornoteblock/AllBlocks.kt
+++ src/main/java/com/panda98277/morerangefornoteblock/AllBlocks.kt
@@ -11,32 +11,18 @@ import net.minecraft.util.Identifier
 
 class AllBlocks {
     companion object {
-        val NOTE_BLOCK_P = NoteBlockBase("noteblock_p", 2, FabricBlockSettings.create().hardness(4.0F))
-        val NOTE_BLOCK_P4 = NoteBlockBase("noteblock_p4", 4, FabricBlockSettings.create().hardness(4.0F))
-        val NOTE_BLOCK_M = NoteBlockBase("noteblock_m", -2, FabricBlockSettings.create().hardness(4.0F))
+        private val NOTE_BLOCK_P = NoteBlockBase("noteblock_p.json", 2, FabricBlockSettings.create().hardness(4.0F))
+        private val NOTE_BLOCK_P4 = NoteBlockBase("noteblock_p4.json", 4, FabricBlockSettings.create().hardness(4.0F))
+        private val NOTE_BLOCK_M = NoteBlockBase("noteblock_m", -2, FabricBlockSettings.create().hardness(4.0F))
         fun registry() {
-            Registry.register(Registries.BLOCK, Identifier(Reference.MODID, NOTE_BLOCK_P.name), NOTE_BLOCK_P);
-            Registry.register(
-                Registries.ITEM,
-                Identifier(Reference.MODID, NOTE_BLOCK_P.name),
-                BlockItem(NOTE_BLOCK_P, FabricItemSettings())
-            );
+            Registry.register(Registries.BLOCK, Identifier(Reference.MODID, NOTE_BLOCK_P.name), NOTE_BLOCK_P)
+            Registry.register(Registries.ITEM, Identifier(Reference.MODID, NOTE_BLOCK_P.name), BlockItem(NOTE_BLOCK_P, FabricItemSettings()))
 
-            Registry.register(Registries.BLOCK, Identifier(Reference.MODID, NOTE_BLOCK_P4.name), NOTE_BLOCK_P4);
-            Registry.register(
-                Registries.ITEM,
-                Identifier(Reference.MODID, NOTE_BLOCK_P4.name),
-                BlockItem(NOTE_BLOCK_P4, FabricItemSettings())
-            );
+            Registry.register(Registries.BLOCK, Identifier(Reference.MODID, NOTE_BLOCK_P4.name), NOTE_BLOCK_P4)
+            Registry.register(Registries.ITEM, Identifier(Reference.MODID, NOTE_BLOCK_P4.name), BlockItem(NOTE_BLOCK_P4, FabricItemSettings()))
 
-            Registry.register(Registries.BLOCK, Identifier(Reference.MODID, NOTE_BLOCK_M.name), NOTE_BLOCK_M);
-            Registry.register(
-                Registries.ITEM,
-                Identifier(Reference.MODID, NOTE_BLOCK_M.name),
-                BlockItem(NOTE_BLOCK_M, FabricItemSettings())
-            );
-
-
+            Registry.register(Registries.BLOCK, Identifier(Reference.MODID, NOTE_BLOCK_M.name), NOTE_BLOCK_M)
+            Registry.register(Registries.ITEM, Identifier(Reference.MODID, NOTE_BLOCK_M.name), BlockItem(NOTE_BLOCK_M, FabricItemSettings()))
         }
 
     }
